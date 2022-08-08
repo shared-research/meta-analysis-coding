c15253f244a2ae14c05e1ac517de06a090ff6ce7
Filippo Gambarota <filippo.gambarota@gmail.com>
2022-08-08 11:46:28 +0200

    cleaning

--- main_script.R
+++ main_script.R
@@ -5,12 +5,6 @@ system("Rscript --encoding=UTF-8 scripts/1_analysis.R")
 system("Rscript --encoding=UTF-8 scripts/2_tables_figures.R")
 system("Rscript --encoding=UTF-8 scripts/3_supplementary.R")
 
-# Update Method
-
-compile_doc("docs/method/method_results.Rmd")
-
 # Update Supplementary
 
 compile_doc("docs/supplementary/supplementary.Rmd")
-
-
